99604411978a23219b863e04d69dfbdd804c7459
anuj <anujkashyap@email.com>
2024-12-20 02:06:49 -0800

    last commit of shery js gooey

---
+++ cat.js
@@ -0,0 +1,31 @@
+
+gsap.from("#img",{
+    x:650,
+    duration:1,
+    opacity:0,
+})
+
+gsap.from("#text",{
+    x:-400,
+    opacity:0,
+    duration:1
+})
+
+gsap.from("#kity h1",{
+    scale:2,
+    duration:1,
+    opacity:0,
+    delay:1
+})
+
+gsap.from("#para",{
+    opacity:0,
+    duration:2,
+    delay:0.8
+})
+
+gsap.from("#nav div",{
+    opacity:0,
+    top:"top 40%",
+    duration:2
+})
